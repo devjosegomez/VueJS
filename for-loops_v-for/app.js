d2d3c46556756b041fcd03a9b52a44acd5265a56
devjosegomez <devjosegomez@gmail.com>
2019-12-22 14:12:16 -0600

    v-for

---
+++ for-loops_v-for/app.js
@@ -0,0 +1,16 @@
+new Vue({
+    el: "#app",
+    data:{
+        classes: "bold",
+        tipos: ["Hamburguesa", "Carnita asada", "Botana", "Refrescos", "Vinos"],
+        team: [
+            {name: "José", age: 27, role: "Developer"},
+            {name: "Paty", age: 27, role: "UX"},
+            {name: "Uriel oaxaca", age: 24, role: "Intern"},
+            {name: "Cristina", age: 29, role: "UX"}
+        ]
+    },
+    methods: {
+    
+    }
+})
